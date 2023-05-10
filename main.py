8556597356986d14adb0f29872f29ebfb61a7d14
Dami Afo <damiafo@MacBook-Air-M2.local>
2023-05-10 07:00:51 +0100

    update code to avoid counting newline character

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ with open("oldLinks.txt", 'r') as openedFile:
     oldLinks = openedFile.readlines()
 with open("newLinks.txt", "w") as newUpdatedLinks:
     for link in oldLinks:
-        totalTweets += 1
-        shortenedLink = link.split('?')[0]
-        newUpdatedLinks.writelines(shortenedLink)
-        newUpdatedLinks.write("\n")
+        if link != "\n":
+            totalTweets += 1
+            shortenedLink = link.split('?')[0]
+            newUpdatedLinks.writelines(shortenedLink)
+            newUpdatedLinks.write("\n")
+        else:
+            continue
 
 
 print(totalTweets,"links has been cleaned up!")
